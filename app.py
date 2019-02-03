baf203bc5b7c5a95265585cac6dc2bfcf1afae76
Lily He <yonglinlilyhe@gmail.com>
2019-02-03 04:20:50 -0500

    display tweet and dominos order details

--- app.py
+++ app.py
@@ -23,16 +23,16 @@ def pizza():
 
     if request.method == "POST":
 
-        text, cord = champion_tweets()
-
+        text, cord, id_str, screen_name = champion_tweets()
+        tweet_link = 'https://twitter.com/' + screen_name + '/status/' + id_str
         lng = str(cord[0])
         lat = str(cord[1])
 
         mapbox_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + lng + "," + lat + ".json?types=poi,address&access_token="
         result = json.loads(requests.get(mapbox_url + config.access_token).content)
 
-        first_name = 'Barack'
-        last_name = 'Obama'
+        first_name = 'ShareMate'
+        last_name = 'App'
         email = ''
         phone_number = '2024561111'
         address = result["features"][0]["place_name"]
@@ -42,16 +42,22 @@ def pizza():
 
         my_local_dominos = StoreLocator.find_closest_store_to_customer(customer)
         order = Order.begin_customer_order(customer, my_local_dominos)
-        for i in range (0, int(request.form["amount"])):
-            order.add_item('P10IGFCZ') # Small (10") Gluten Free Crust Wisconsin 6 Cheese Pizza $12.99
 
+        list_of_order = [];
+        for i in range (0, int(request.form["amount"])):
+            list_of_order.append(order.add_item('P10IGFCZ'))
         card = CreditCard('4100123422343234', '0115', '777', '90210')
 		# my_local_dominos.place_order(order, card)
-        
-        print(text)
-        print(order)
 
-    return render_template("success.html")
+        res = {'Tweet': {'text': text, 'link': tweet_link}, 'Geolocation': cord, 'Address': address, 'Local Dominos': my_local_dominos, 'Total Order': list_of_order}
+
+        # When Dominos locations are closed
+        # res = {'Tweet': {'text': text, 'link': tweet_link}, 'Geolocation': cord, 'Address': address}
+    return render_template("success.html", result=res)
+
+@app.route("/complete")
+def complete():
+    return render_template("complete.html")
 
 if __name__ == "__main__":
     app.run()
