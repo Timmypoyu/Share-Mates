5345ed9b7fe64923190cfb68d7fcb8de702daba6
Brian Yang <by2225@columbia.edu>
2019-02-03 02:04:03 -0500

    Able to display coordinates and text of tweets

--- tweeter_bot.py
+++ tweeter_bot.py
@@ -1,43 +1,48 @@
 import tweepy
 import time
-import config
+#import config
 import requests
-from datatime import datetime, timedelta, timezone
+from datetime import datetime, timedelta, timezone
 
 
-CONSUMER_KEY = config.consumer_key
-CONSUMER_SECRET_KEY = config.consumer_secret_key
-ACCESS_KEY = config.access_key
-ACCESS_SECRET = config.access_secret
+CONSUMER_KEY = "nFpgZnq2KPNNA4wb6JwqpT6tm"
+CONSUMER_SECRET_KEY = "3ZolVazwhTdW6xRSlfl2Zsnt5VjCRgdGi6xlFTCCOMgSnjrCfW"
+ACCESS_KEY = "946302291300290560-KksVRF89EN6RsO0LYqKHzYZnrBAgnbO"
+ACCESS_SECRET = "EFz3IE3PA8dI5W3kkzJQOV7HZ4krHm0hllSPaalorPThO"
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 def champion_tweets():
-    print('finding best tweets...'. flush=True)
+    print('finding best tweets...', flush=True)
     mentions = api.mentions_timeline()
 
     count = 0
+    text= ""
+    cord= []
     for mention in mentions:
-        temp_time = datetime.strptime(mention.created_at, "%a %b %d %H:%M:%S %z %Y") 
+        temp_time = datetime.strptime(str(mention.created_at), "%Y-%m-%d %H:%M:%S") 
         
-        if ((temp_time > datetime.now(timezone.utc) - timedelta(hours = 2)) and mention.user.geo_enabled ):
+        if ((temp_time > (datetime.now() - timedelta(hours = 2))) and mention.user.geo_enabled ):  
             if '#sendpizza' in mention.text.lower():
-                if (count < mention.favorited):
+               
+                if (count <= mention.favorited):
+                   
                     count = mention.favorited
                     text = mention.text
-                    cord = mention.coordinates.coordinates
+                    cord = mention.coordinates["coordinates"]
+               
                                 
     return text, cord
 
 
-text, cord = champion_tweets()
-r = requests.post("http://localhost:5000/pizza", json={"text": text, "cord": cord})
+print(champion_tweets())
+# r = requests.post("http://localhost:5000/pizza", json={"text": text, "cord": cord})
 
-if (r.status_code == 200):
-    print("YEAH!")
-else:
-    print("OH NO!")
+# if (r.status_code == 200):
+#     print("YEAH!")
+# else:
+#     print("OH NO!")
 
 
