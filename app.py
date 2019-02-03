f154fa106412bf86fecee0335d4bfb129c8e326c
Hussein Fardous <hf2334@columbia.edu>
2019-02-03 01:59:45 -0500

    Connected Frontend to Backend

--- app.py
+++ app.py
@@ -7,17 +7,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/pizza", methods = ["GET"])
+@app.route("/food")
+def food():
+    return render_template("food.html")
+
+@app.route("/pizza", methods = ["POST"])
 def pizza():
 
-    if request.method == "GET":
+    if request.method == "POST":
 
-        try:
+    	print(request.form["amount"])
 
-            print("Success")
-
-        except Exception as e:
-            return render_template("index.html", text = str(e))
+    return render_template("success.html")
 
 if __name__ == "__main__":
     app.run()
--- static/func.js
+++
@@ -1,7 +0,0 @@
-
-function foodRedirect() {
-    window.location = "food.html";
-}
-function homePage(){
-    window.location = "index.html";
-}
