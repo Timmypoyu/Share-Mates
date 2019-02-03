750db094a75f4b344d41e96f95107a481c6780f7
Lily He <yonglinlilyhe@gmail.com>
2019-02-03 02:42:12 -0500

    use config variables

--- tweeter_bot.py
+++ tweeter_bot.py
@@ -1,14 +1,14 @@
 import tweepy
 import time
-#import config
+import config
 import requests
 from datetime import datetime, timedelta, timezone
 
 
-CONSUMER_KEY = "nFpgZnq2KPNNA4wb6JwqpT6tm"
-CONSUMER_SECRET_KEY = "3ZolVazwhTdW6xRSlfl2Zsnt5VjCRgdGi6xlFTCCOMgSnjrCfW"
-ACCESS_KEY = "946302291300290560-KksVRF89EN6RsO0LYqKHzYZnrBAgnbO"
-ACCESS_SECRET = "EFz3IE3PA8dI5W3kkzJQOV7HZ4krHm0hllSPaalorPThO"
+CONSUMER_KEY = config.consumer_key
+CONSUMER_SECRET_KEY = config.consumer_secret_key
+ACCESS_KEY = config.access_key
+ACCESS_SECRET = config.access_secret
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -22,16 +22,16 @@ def champion_tweets():
     text= ""
     cord= []
     for mention in mentions:
-        temp_time = datetime.strptime(str(mention.created_at), "%Y-%m-%d %H:%M:%S") 
-        
-        if ((temp_time > (datetime.now() - timedelta(hours = 2))) and mention.user.geo_enabled ):  
+        temp_time = datetime.strptime(str(mention.created_at), "%Y-%m-%d %H:%M:%S")
+
+        if ((temp_time > (datetime.now() - timedelta(hours = 2))) and mention.user.geo_enabled ):
             if '#sendpizza' in mention.text.lower():
-               
+
                 if (count <= mention.favorited):
-                   
+
                     count = mention.favorited
                     text = mention.text
                     cord = mention.coordinates["coordinates"]
-               
-                                
+
+
     return text, cord
