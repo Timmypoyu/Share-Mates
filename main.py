a4637884b120648d9e89e04ad74b5a0a6a085380
Hussein Fardous <hf2334@columbia.edu>
2019-02-02 23:38:11 -0500

    Reverse Geocode via Mapbox

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import sys
+import requests
+import json
 sys.path.append('./pizzapi')
 import pizzapy
 from pizzapy import *
+import config
+
+lng = "-73.989"
+lat = "40.733"
+
+mapbox_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + lng + "," + lat + ".json?types=poi,address&access_token="
+result = json.loads(requests.get(mapbox_url + config.access_token).content)
 
 first_name = 'Barack'
 last_name = 'Obama'
 email = ''
 phone_number = '2024561111'
-address = '700 Pennsylvania Avenue NW, Washington, DC, 20408'
+address = result["features"][0]["place_name"]
+if address[-13:] == "United States":
+	address = address[:-15]
 customer = Customer(first_name, last_name, email, phone_number, address)
 
 my_local_dominos = StoreLocator.find_closest_store_to_customer(customer)
@@ -18,3 +29,4 @@ order.add_item('P10IGFCZ') # Small (10") Gluten Free Crust Wisconsin 6 Cheese Pi
 card = CreditCard('4100123422343234', '0115', '777', '90210')
 # my_local_dominos.place_order(order, card)
 print("Success")
+print(result["features"][0]["place_name"][:-15])
